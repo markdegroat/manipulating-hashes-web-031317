12617f86e7f235899886809a36339a9ef5a3cdbe
Mark deGroat <mhdegroat@gmail.com>
2017-03-10 18:11:43 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -5,9 +5,11 @@ def second_challenge
    meat: ["chicken", "steak", "salmon"],
    grains: ["rice", "pasta"]
   }
-
+  array_to_return = []
   #code your solution here!
-
-  
+  #groceries.each do |key, value|
+    array_to_return += groceries.values.flatten
+  #end
+  array_to_return
 
 end
